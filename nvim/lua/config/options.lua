466ee6499bfb1a24253795951a880405724fe030
gabkhanfig <gabkhanfig@gmail.com>
2026-05-23 23:20:11 -0400

    word wrap

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker_hidden = true
+
+vim.o.wrap = true
+vim.o.textwidth = 100
